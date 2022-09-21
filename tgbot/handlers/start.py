1b8b7899d2735b5bb011b397c164687aae1ca60f
Tim <herew26@gmail.com>
2022-09-22 02:10:49 +0300

    feat: add welcome text

--- tgbot/handlers/start.py
+++ tgbot/handlers/start.py
@@ -1,15 +1,26 @@
+from asyncio import sleep
+
 from aiogram import Dispatcher, types
 from aiogram.dispatcher.filters import CommandStart
+from aiogram.utils.markdown import hlink
 
+from tgbot.config import Config
 from tgbot.models.user_tg import UserTG
 
 
-async def start(message: types.Message, user: UserTG) -> None:
-    text = f"""
-<b>Привет{f', {user.info}' if user.info else ''}!</b>
-"""
+async def start(message: types.Message, user: UserTG, config: Config) -> None:
+    start_text = (
+        "Привет!",
+        "Я могу <b>убрать одежду</b> с любой фотографии девушки 🥰",
+        "Не переживай, все <b>конфиденциально</b> и <b>анонимно</b> 🙈",
+        f"Можешь посмотреть на {hlink('примеры', config.misc.photo_examples_url)} 🔞",
+        "<b>Отправь мне фотографию</b> девушки и я уберу с нее одежду 👙"
+    )
 
-    await user.send_message(text)
+    for text in start_text:
+        await user.send_chat_action(types.ChatActions.TYPING)
+        await sleep(2)
+        await user.send_message(text)
 
 
 def register(dp: Dispatcher) -> None:
